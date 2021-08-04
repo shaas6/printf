d0a7a9174e4b27e7d51b04dc89b5f7b8f20688a7
Weltopa <patrick.welton@yahoo.com>
2021-08-04 08:20:29 -0700

    Print percent add

---
+++ printf_percent.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * printf_percent - prints % sign
+ */
+
+int printf_percent(va_list __attribute__((unused)) arg)
+{
+	char x = '%';
+
+	_putchar(x);
+
+	return (0);
+}
